35110fc7015e6aaec21a3ba4462a9ec9a1c0374b
jesse-zonneveld <jesse.l.zonneveld@gmail.com>
2020-07-13 23:49:49 -0700

    add local storage functionality and css properties

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-let myLib = [];
-const addBookButton = document.querySelector('.addBook');
+const myLib = JSON.parse(localStorage.getItem('myLib')) || [];
+let libraryContainer = document.querySelector('.library');
+const addBookButton = document.querySelector('.add-book');
 const form = document.querySelector('.new-book-info');
 const lightbox = document.querySelector('.lightbox');
 const dimmer = document.createElement('div');
 
 addBookButton.addEventListener('click', displayPopup);
 form.addEventListener('submit', addBookToLib);
+libraryContainer.addEventListener('click', deleteBook);
+libraryContainer.addEventListener('click', toggleRead);
 
 function displayPopup() {
     lightbox.style.visibility = 'visible';
@@ -17,13 +20,12 @@ function displayPopup() {
     dimmer.className = 'dimmer';
     document.body.appendChild(dimmer);
 
-    dimmer.addEventListener('click', () => {
+    dimmer.addEventListener('click', (e) => {
+        if (!e.target.parentElement.matches('body') || e.target === null) return;
         lightbox.style.visibility = 'hidden';
         document.body.removeChild(dimmer);
     });
 }
-
-
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -45,21 +47,46 @@ function addBookToLib(e) {
     lightbox.style.visibility = 'hidden';
     document.body.removeChild(dimmer);
     renderLibrary();
+    localStorage.setItem('myLib', JSON.stringify(myLib));
 }
 
 function renderLibrary() {
-    document.querySelector('.library').innerHTML = myLib.map(book => {
-        return `<div class="book">
+    libraryContainer.innerHTML = myLib.map((book, i) => {
+        console.log(book, i);
+        return `<div class="book" data-index="${i}">
                     <div class="title">${book.title}</div>
-                    <div class="author">${book.author}</div>
-                    <div class="pages">${book.pages}</div>
-                    <div class="read">Done
+                    <div class="author">By:  ${book.author}</div>
+                    <div class="pages">pages:  ${book.pages}</div>
+                    <div class="read">${book.read === true ? 'Complete' : 'Incomplete'} 
                         <label class="switch">
                         <input type="checkbox" ${book.read === true ? 'checked' : ''}>
                         <span class="slider round"></span>
                         </label>
                     </div>
+                    <button class="delete">Delete</button>
                 </div>
         `;
     }).join('\n');
 }
+
+function deleteBook(e) {
+    if (!e.target.matches('button')) return;
+    index = e.target.parentElement.dataset.index;
+    console.log(index);
+    myLib[index] = myLib[myLib.length - 1];
+    myLib.pop();
+    renderLibrary();
+    localStorage.setItem('myLib', JSON.stringify(myLib));
+}
+
+function toggleRead(e) {
+    if (!e.target.matches('.slider')) return;
+    index = e.target.parentElement.parentElement.parentElement.dataset.index;
+    console.log(index);
+    myLib[index].read = !myLib[index].read;
+    console.log(myLib[index]);
+    localStorage.setItem('myLib', JSON.stringify(myLib));
+    renderLibrary();
+}
+
+renderLibrary();
